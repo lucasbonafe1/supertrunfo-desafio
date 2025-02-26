47ce52b17d14d90defca656159abddf250f8c2ee
Lucas Bonafé <l2005bonafe@gmail.com>
2025-02-26 20:07:44 -0300

    feat: add logica

--- init.c
+++ init.c
@@ -1,45 +1,68 @@
 #include <stdio.h>
 
-typedef struct carta = {
-    char state;
-    string letterCode;
-    string city;
-    int population
-    float area;
-    float pib;
-    int turisticPoints
-}
+typedef struct {
+    char state[3];         
+    char letterCode[10];   
+    char city[50];         
+    int population;    
+    float area;            
+    float pib;             
+    int turisticPoints;    
+} Carta;
 
 int main(){
+    Carta carta1;
+    Carta carta1;
+
     printf("Escreva o estado da carta: ");
-    scanf("%s", carta.state)
+    scanf("%s", carta1.state)
 
     printf("Escreva o código da carta: ");
-    scanf("%s", carta.letterCode);
+    scanf("%s", carta1.letterCode);
 
     printf("Escreva a cidade da carta: ");
-    scanf("%s", carta.city);
+    scanf("%s", carta1.city);
 
     printf("Digite a população: ");
-    scanf("%d", &carta.population);
+    scanf("%d", &carta1.population);
 
     printf("Digite a área: ");
-    scanf("%f", &carta.area);
+    scanf("%f", &carta1.area);
 
     printf("Digite o PIB: ");
-    scanf("%f", &carta.pib);
+    scanf("%f", &carta1.pib);
 
     printf("Digite o número de pontos turísticos: ");
-    scanf("%d", &carta.turisticPoints);
+    scanf("%d", &carta1.turisticPoints);
 
-    printf("\nDados da carta:\n");
-    printf("Estado: %s\n", carta.state);
-    printf("Código: %s\n", carta.letterCode);
-    printf("Cidade: %s\n", carta.city);
-    printf("População: %d\n", carta.population);
-    printf("Área: %.2f\n", carta.area);
-    printf("PIB: %.2f\n", carta.pib);
-    printf("Pontos turísticos: %d\n", carta.turisticPoints);
+    printf("Agora vamos para a segunda carta: \n");
+
+    printf("Escreva o estado da carta: ");
+    scanf("%s", carta2.state)
+
+    printf("Escreva o código da carta: ");
+    scanf("%s", carta2.letterCode);
+
+    printf("Escreva a cidade da carta: ");
+    scanf("%s", carta2.city);
+
+    printf("Digite a população: ");
+    scanf("%d", &carta2.population);
+
+    printf("Digite a área: ");
+    scanf("%f", &carta2.area);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &carta2.pib);
+
+    printf("Digite o número de pontos turísticos: ");
+    scanf("%d", &carta2.turisticPoints);
+
+   if(carta1.area > carta2.area){
+    printf("Valor da área da carta 1 é maior, portanto CARTA 1 venceu!");
+   } else {
+    printf("Valor da área da carta 2 é maior, portanto CARTA 2 venceu!");
+   }
     
     return 0;
 }
